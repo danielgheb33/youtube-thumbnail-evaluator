6ac1d218d50b90d9e17db24f7ed8dddbc84bf62a
Daniel Ghebreyesus <daniel_ghebreyesus@brown.edu>
2023-10-06 22:01:45 -0700

    Final

---
+++ main.py
@@ -0,0 +1,182 @@
+import sys
+import pickle
+import numpy as np
+import tensorflow as tf
+from models.enhanced_model import EnhancedModel, accuracy_function, loss_function
+from models.combined_models import ImageNumModel, ImageTextModel, NumTextModel
+from models.simple_models import ImageModel, TextModel, NumModel, SimpleModel
+
+
+def train(model, train_images, train_text, train_nums, train_views, batch_size=10):
+    '''
+    Originally, we used MSE as our loss function, however, due to the variance in our data, 
+    some views in the hundreds of millions and others in the low thousands, we wanted our 
+    model to try and minimize relative loss as opposed to an absolute metric. This is why 
+    the accuracy and loss functions are the same, hence only accuracy is reported, not loss.
+    '''
+    # total_loss = 0
+    total_acc = 0
+    num_batches = int(len(train_images) / batch_size)
+
+    for _, end in enumerate(range(batch_size, len(train_images) + 1, batch_size)):
+        start = end - batch_size
+        batch_images = train_images[start:end, :]
+        batch_text = train_text[start:end, :]
+        batch_nums = train_nums[start:end, :]
+        batch_views = train_views[start:end]
+
+        with tf.GradientTape() as tape:
+            preds = model(batch_images, batch_text, batch_nums)
+            loss = loss_function(preds, batch_views)
+            acc = accuracy_function(preds, batch_views)
+
+        gradients = tape.gradient(loss, model.trainable_variables)
+        model.optimizer.apply_gradients(
+            zip(gradients, model.trainable_variables))
+        # total_loss += loss
+        total_acc += acc
+
+    # avg_loss = total_loss / num_batches
+    avg_acc = total_acc / num_batches
+    # return avg_loss.numpy(), avg_acc.numpy()
+    return avg_acc.numpy()
+
+def test(model, test_images, test_text, test_nums, test_views):
+    preds = model(test_images, test_text, test_nums)
+    acc = accuracy_function(preds, test_views)
+    return acc.numpy()
+
+def main(desired_model, desired_learning_rate, desired_batch_size, desired_epochs):
+
+    file_path = './data/data.p'
+    with open(file_path, 'rb') as data_file:
+        data_dict = pickle.load(data_file)
+    train_images = np.array(data_dict['train_images'])
+    test_images = np.array(data_dict['test_images'])
+    train_text = np.array(data_dict['train_text'])
+    test_text = np.array(data_dict['test_text'])
+    train_nums = np.array(data_dict['train_nums'])
+    test_nums = np.array(data_dict['test_nums'])
+    train_views = np.array(data_dict['train_views'])
+    test_views = np.array(data_dict['test_views'])
+    word2idx = data_dict['word2idx']
+
+    model = None
+    match desired_model:
+        case 'EnhancedModel':
+            model = EnhancedModel(4096, 5, 128, len(word2idx), 50, desired_learning_rate)
+        case 'ImageNumModel':
+            model = ImageNumModel(4096, 5, 128, len(word2idx), 50, desired_learning_rate)
+        case 'ImageTextModel':
+            model = ImageTextModel(4096, 5, 128, len(word2idx), 50, desired_learning_rate)
+        case 'NumTextModel':
+            model = NumTextModel(4096, 5, 128, len(word2idx), 50, desired_learning_rate)
+        case 'ImageModel':
+            model = ImageModel(4096, 5, 128, len(word2idx), 50, desired_learning_rate)
+        case 'NumModel':
+            model = NumModel(4096, 5, 128, len(word2idx), 50, desired_learning_rate)
+        case 'TextModel':
+            model = TextModel(4096, 5, 128, len(word2idx), 50, desired_learning_rate)
+        case 'SimpleModel':
+            model = SimpleModel(4096, 5, 128, len(word2idx), 50, desired_learning_rate)
+
+
+    epochs = desired_epochs
+    print("---------------------------TRAIN-----------------------------")
+    acc = [0] * epochs
+    stable_count = 0
+    stable_accuracy = 0
+    for i in range(epochs):
+        acc[i] = train(model, train_images, train_text, train_nums, train_views, desired_batch_size)
+        print(f"---------------------------EPOCH {i}---------------------------")
+        print(acc[i])
+        print("-------------------------------------------------------------")
+        '''
+        In order to speed up training, we have implemented a cut-off function that ends training
+        so that we don't needlessly train our models wasting time or overfit the training data.
+        Due to our previous experieces with how our models perform and learn, plus or minus 2.5%
+        accuracy for 4 epochs was deemed a plateau in minimizing loss.
+        '''
+        if (acc[i] < stable_accuracy + 2.5) and (acc[i] > stable_accuracy - 2.5):
+            stable_count += 1
+        else:
+            stable_count = 0
+            stable_accuracy = acc[i]
+        if stable_count >= 2:
+            break
+    print("---------------------------TEST------------------------------")
+    print(test(model, test_images, test_text, test_nums, test_views))
+
+
+if __name__ == "__main__":
+
+    n = len(sys.argv)
+
+    # default parameters
+    if n == 1:
+        desired_model = 'EnhancedModel'
+        desired_learning_rate = 0.1
+        desired_batch_size = 10
+        desired_epochs = 25
+        main(desired_model, desired_learning_rate, desired_batch_size, desired_epochs)
+        exit()
+    
+    if n != 5:
+        print("usage: python main.py [model] [learning rate] [batch size] [epochs]")
+        print("example: python main.py EnhancedModel 0.1 10 25")
+        exit()
+
+    models = ['EnhancedModel', 'ImageNumModel', 'ImageTextModel', 'NumTextModel', 'ImageModel', 'NumModel', 'TextModel', 'SimpleModel']
+    desired_model = sys.argv[1]
+    if desired_model not in models:
+        print("usage: python main.py [model] [learning rate] [batch size] [epochs]")
+        print("example: python main.py EnhancedModel 0.1 10 25")
+        print("[model] must be one of the following: ")
+        for model in models:
+            print(model)
+        exit()
+
+    try:
+        desired_learning_rate = float(sys.argv[2])
+    except:
+        print("usage: python main.py [model] [learning rate] [batch size] [epochs]")
+        print("example: python main.py EnhancedModel 0.1 10 25")
+        print("[learning rate] must be a float")
+        exit()
+    if desired_learning_rate < 0 or desired_learning_rate > 1:
+        print("usage: python main.py [model] [learning rate] [batch size] [epochs]")
+        print("example: python main.py EnhancedModel 0.1 10 25")
+        print("[learning rate] must be a float between 0 and 1")
+        exit()
+
+    try:
+        desired_batch_size = int(sys.argv[3])
+        file_path = './data/data.p'
+        with open(file_path, 'rb') as data_file:
+            data_dict = pickle.load(data_file)
+        max_batch_size = len(data_dict['train_images'])
+    except:
+        print("usage: python main.py [model] [learning rate] [batch size] [epochs]")
+        print("example: python main.py EnhancedModel 0.1 10 25")
+        print("[batch size] must be an integer")
+        exit()
+    if desired_batch_size < 1 or desired_batch_size > max_batch_size:
+        print("usage: python main.py [model] [learning rate] [batch size] [epochs]")
+        print("example: python main.py EnhancedModel 0.1 10 25")
+        print(f"[batch size] must be an integer greater than 0 and less than {max_batch_size}")
+        exit()
+
+    try:
+        desired_epochs = int(sys.argv[4])
+    except:
+        print("usage: python main.py [model] [learning rate] [batch size] [epochs]")
+        print("example: python main.py EnhancedModel 0.1 10 25")
+        print("[batch size] must be an integer")
+        exit()
+    if desired_epochs < 1 or desired_epochs > 100:
+        print("usage: python main.py [model] [learning rate] [batch size] [epochs]")
+        print("example: python main.py EnhancedModel 0.1 10 25")
+        print("[epochs] must be an integer greater than 0 and less than 100")
+        exit()
+
+    main(desired_model, desired_learning_rate, desired_batch_size, desired_epochs)
---
+++ preprocess.py
@@ -0,0 +1,174 @@
+import re
+import sys
+import pickle
+import requests
+import datetime
+import collections
+import numpy as np
+import pandas as pd
+from PIL import Image
+from io import BytesIO
+import tensorflow as tf
+
+
+'''
+preprocess_images() takes in a list of links to thumbnails of YouTube videos
+and retrieves the thumbnails from the links at which they are stored.
+'''
+def preprocess_images(thumbnail_urls):
+    preprocessed_images = [None] * thumbnail_urls.shape[0]
+    for i, url in enumerate(thumbnail_urls):
+        image_data = requests.get(url).content
+        image = Image.open(BytesIO(image_data))
+        preprocessed_images[i] = np.asarray(image.getdata(), dtype=np.float32).reshape(120, 90, 3)
+    return preprocessed_images
+
+'''
+preprocess_text() takes in the titles of videos as well as the hashtags associated
+with the video, strips them of any formatting, and concatenates all the words into 
+strings of length window_size. The decision to add the hashtags before video titles 
+was arbitrary. This does imply that for videos with lots of hashtags or longer video
+titles, the title will be truncated before hashtags are.
+'''
+def preprocess_text(titles, hashtags, window_size=50):
+    preprocessed_text = [None] * titles.shape[0]
+    for i, (hashtag, title) in enumerate(zip(hashtags, titles)):
+        text_old_format = hashtag + " " + title
+        text_unformatted = re.sub(r"[^a-zA-Z0-9]+", ' ', text_old_format.lower())
+        text_unformatted = [word for word in text_unformatted.split() if ((len(word) > 1) and (word.isalpha()))]
+        text_new_format = ['<start>'] + text_unformatted[:window_size-1] + ['<end>']
+        preprocessed_text[i] = text_new_format
+    return preprocessed_text
+
+'''
+preprocess_dates() takes in a list of the dates on which videos were published and
+converts that information to a standardized format, albeit normalized.
+'''
+def preprocess_dates(dates):
+    preprocessed_dates = [None] * dates.shape[0]
+    for i, date in enumerate(dates):
+        iso_date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z')
+        utc_date = datetime.datetime.fromisoformat(str(iso_date)).replace(tzinfo=datetime.timezone.utc)
+        unix_timestamp = int(utc_date.timestamp())
+        preprocessed_dates[i] = unix_timestamp
+    return preprocessed_dates
+
+'''
+preprocess_data() takes in the number of data points a user wants to use to train their 
+model and their desired train-test data split. It then reads that data from a csv file
+containing data on 200,000 YouTube videos, and then stores all the relevant data as a 
+dictionary in a pickle file. The data is normalized before being stored as the dictionary.
+'''
+def preprocess_data(desired_range, desired_split):
+    # data source: https://www.kaggle.com/datasets/rsrishav/youtube-trending-video-dataset/versions/1000
+    data_file = r'./data/data.csv'
+    specific_rows = [i for i in range(desired_range)]
+    specific_data = pd.read_csv(data_file, skiprows=lambda x: x not in specific_rows)
+
+    thumbnails = preprocess_images(specific_data['thumbnail_link'])
+    text = preprocess_text(specific_data['title'], specific_data['tags'])
+    dates = preprocess_dates(specific_data['publishedAt'])
+    likes = specific_data['likes']
+    views = specific_data['view_count']
+
+    numbers = np.concatenate([tf.expand_dims(dates, axis=1), tf.expand_dims(likes, axis=1)], axis=1)
+
+    ids = specific_data['video_id']
+    num_samples = int(ids.shape[0])
+    train_num = int(desired_split * num_samples)
+    train_text = text[:train_num]
+    test_text = text[train_num:]
+    word_count = collections.Counter()
+    for words in train_text:
+        word_count.update(words)
+
+    def unk_text(texts, minimum_frequency):
+        for text in texts:
+            for index, word in enumerate(text):
+                if word_count[word] <= minimum_frequency:
+                    text[index] = '<unk>'
+    unk_text(train_text, 10)
+    unk_text(test_text, 10)
+
+    def pad_captions(captions, window_size=50):
+        for caption in captions:
+            caption += (window_size + 1 - len(caption)) * \
+                ['<pad>']
+    pad_captions(train_text)
+    pad_captions(test_text)
+
+    word2idx = {}
+    vocab_size = 0
+
+    for caption in train_text:
+        for index, word in enumerate(caption):
+            if word in word2idx:
+                caption[index] = word2idx[word]
+            else:
+                word2idx[word] = vocab_size
+                caption[index] = vocab_size
+                vocab_size += 1
+
+    for caption in test_text:
+        for index, word in enumerate(caption):
+            caption[index] = word2idx[word]
+
+    return dict(train_images=np.array(thumbnails[:train_num]),
+                test_images=np.array(thumbnails[train_num:]),
+                train_text=np.array(train_text),
+                test_text=np.array(test_text),
+                train_nums=np.array(numbers[:train_num]),
+                test_nums=np.array(numbers[train_num:]),
+                train_views=np.array(views[:train_num]),
+                test_views=np.array(views[train_num:]),
+                word2idx=word2idx,
+                idx2word={v: k for k, v in word2idx.items()})
+
+
+if __name__ == '__main__':
+
+    n = len(sys.argv)
+
+    # default parameters
+    if n == 1:
+        desired_range = 1000
+        desired_split = 0.7
+        with open(f'data/data.p', 'wb') as pickle_file:
+            pickle.dump(preprocess_data(desired_range, desired_split), pickle_file)
+        exit()
+    
+    if n != 3:
+        print("usage: python preprocess.py [number of data points] [percentage of data for training]")
+        print("example: python preprocess.py 1000 0.7")
+        exit()
+
+    try:
+        desired_range = int(sys.argv[1])
+    except:
+        print("usage: python preprocess.py [number of data points] [percentage of data for training]")
+        print("example: python preprocess.py 1000 0.7")
+        print("[number of data points] must be an integer")
+        exit()
+    if desired_range < 1 or desired_range > 200000:
+        print("usage: python preprocess.py [number of data points] [percentage of data for training]")
+        print("example: python preprocess.py 1000 0.7")
+        print("[number of data points] must be an integer greater than 0 and less than 200000")
+        exit()
+
+    try:
+        desired_split = float(sys.argv[2])
+    except:
+        print("usage: python preprocess.py [number of data points] [percentage of data for training]")
+        print("example: python preprocess.py 1000 0.7")
+        print("[percentage of data for training] must be a float")
+        exit()
+    if desired_split < 0 or desired_split > 1:
+        print("usage: python preprocess.py [number of data points] [percentage of data for training]")
+        print("example: python preprocess.py 1000 0.7")
+        print("[percentage of data for training] must be a float between 0 and 1")
+        exit()
+
+    with open(f'data/data.p', 'wb') as pickle_file:
+        pickle.dump(preprocess_data(desired_range, desired_split), pickle_file)
+
+    print(f'data has been dumped into data/data.p!')
